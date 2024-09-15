00bfcb199f7ea113bf99d599bf3ee1667434e41b
KineshLohar <kineshmalviya@gmail.com>
2024-09-15 12:26:58 +0530

    tuples

---
+++ Tuples/tuples.ts
@@ -0,0 +1,22 @@
+const testData : [number, string] = [1, 'sdsfsdf']
+
+type User = {
+    name : string;
+    id :number;
+}
+
+let users : [User, boolean, string]
+
+users = [
+    {
+        name : 'koes',
+        id : 1
+    },
+    true,
+    'sdsd'
+]
+
+
+let numbers : [number, number, number] = [1,2,3]
+
+numbers[2] = 4
